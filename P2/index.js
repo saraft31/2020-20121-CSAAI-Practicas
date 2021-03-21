0ccd3f59b3e4afa6f131e3ccaab54bfbb712121b
saraft <s.fernandezt.2017@alumnos.urjc.es>
2021-03-21 23:17:15 +0100

    suma, resta, %

--- P2/index.js
+++ P2/index.js
@@ -4,12 +4,17 @@ console.log("Ejecutando JS...");
 // Elementos de la interfaz de la calculadora
 display = document.getElementById("display")
 
+exp = document.getElementById("exp")
 suma = document.getElementById("suma")
 igual = document.getElementById("igual")
 clear = document.getElementById("clear")
 
 // -- Insertar digitos
+//-- Crea un array con todos los elementos de la clase cdigito
 let digitos = document.getElementsByClassName("digito")
+//-- Crea un array con todos los elementos de la clase operacion
+//let operacion = document.getElementsByClassName("operacion");
+
 
 for(i=0; i<digitos.length; i++){
     digitos[i].onclick = (ev) =>{
@@ -19,9 +24,9 @@ for(i=0; i<digitos.length; i++){
 
 function digito(button) {
     if(display.innerHTML=="0"){
-        display.innerHTML = button.id;
+        display.innerHTML = button.value;
     }else{
-        display.innerHTML += button.id;
+        display.innerHTML += button.value;
     }
 }
 
@@ -33,7 +38,11 @@ suma.onclick = () => {
 
 resta.onclick = () => {
     display.innerHTML += "-";
-  }
+}
+
+exp.onclick = () => {
+    display.innerHTML += "^";
+}
 
 //-- Evaluar la expresion
 igual.onclick = () => {
