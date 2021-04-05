3ecfdfe2870f772dd51117decc04eb5d27968cfe
saraft <s.fernandezt.2017@alumnos.urjc.es>
2021-04-05 21:15:40 +0200

    añado sonido

--- P2/index.js
+++ P2/index.js
@@ -8,13 +8,13 @@ clear = document.getElementById("clear")
 borra_ultimo = document.getElementById("borra_ultimo")
 onOFF = document.getElementById("onoff")
 
-// -- Insertar digitos
-//-- Crea un array con todos los elementos de la clase cdigito
+//  Insertar digitos
+// Crea un array con todos los elementos de la clase cdigito
 let digitos = document.getElementsByClassName("digito")
-//-- Crea un array con todos los elementos de la clase operacion
+// Crea un array con todos los elementos de la clase operacion
 let operacion = document.getElementsByClassName("operacion");
 
-//--Estados de la calculadora
+// Estados de la calculadora
 const ESTADO = {
     INIT: 0,
     OP1: 1,
@@ -44,31 +44,37 @@ for(i=0; i<operacion.length; i++){
     }
 }
 
-//-- Comprobacion de estado de digitos
+// Comprobacion de estado de digitos
 function digito(boton){
-    //-- Segun el estado hacemos una cosa u otra
-    if(estado == ESTADO.INIT){
-      display.innerHTML = boton;
-      estado = ESTADO.OP1;
-    }else if (estado == ESTADO.OP1){
-      display.innerHTML += boton;
-    } else if (estado == ESTADO.OPERATION) {
-      display.innerHTML += boton;
-      estado = ESTADO.OP2_INIT;
-    }else if (estado == ESTADO.OP2_INIT) {
-      display.innerHTML +=  boton;
-      estado = ESTADO.OP2;
-    }else if (estado == ESTADO.OP2){
-      display.innerHTML += boton;
+    // Segun el estado hacemos una cosa u otra
+    if(currentvalue == "On"){
+      if(estado == ESTADO.INIT){
+        display.innerHTML = boton;
+        estado = ESTADO.OP1;
+      }else if (estado == ESTADO.OP1){
+        display.innerHTML += boton;
+      } else if (estado == ESTADO.OPERATION) {
+        display.innerHTML += boton;
+        estado = ESTADO.OP2_INIT;
+      }else if (estado == ESTADO.OP2_INIT) {
+        display.innerHTML +=  boton;
+        estado = ESTADO.OP2;
+      }else if (estado == ESTADO.OP2){
+        display.innerHTML += boton;
+      }
+      bip.play();
     }
   }
 
-//-- Comprobacion de estado de las operaciones
+// Comprobacion de estado de las operaciones
 function operaciones(operacion){
-    //-- Segun el estado hacemos una cosa u otra
-    if (estado != ESTADO.OPERATION) {
-      display.innerHTML += operacion;
-      estado = ESTADO.OPERATION;
+    // Segun el estado hacemos una cosa u otra
+    if(currentvalue == "On"){
+      if (estado != ESTADO.OPERATION) {
+        display.innerHTML += operacion;
+        estado = ESTADO.OPERATION; 
+        bip.play();
+      }
     }
   }
 
@@ -87,42 +93,54 @@ function onoff(){
     document.getElementById("display").style.backgroundColor = "black";
     document.getElementById("onoff").style.backgroundColor = "white";
   }
+  bip.play();
 }
 
-function play(){
-  currentvalue = document.getElementById('onoff').value;
-  if(currentvalue == "Off"){
-    
-  }
-}
+
 
 // Poner el display electronicos
-// 
 
-//-- Borra el ultimo digito u operando
+// Borra el ultimo digito u operando
 borra_ultimo.onclick = (ev) => {
+  if(estado == ESTADO.INIT || currentvalue == "Off" ){
+    display.innerHTML = "0";
+    
+  }else{
+    estado = ESTADO.OPERATION;
     display.innerHTML = display.innerHTML.slice(0,-1);
     console.log(estado,"borrar digito");
+    bip.play();
+  }
+    
   }
 
-//-- Poner una coma
+// Poner una coma
 coma.onclick = () => {
+  if(currentvalue == "On"){
     if(estado == ESTADO.OP1 || estado == ESTADO.OP2_INIT || estado == ESTADO.INIT){
       display.innerHTML += coma.innerHTML;
       estado = ESTADO.OPERATION;
       console.log(estado, "poner coma");
+      bip.play();
     }
   }
+  }
 
-//-- Evaluar la expresion
+// Evaluar la expresion
 igual.onclick = () => {
-  display.innerHTML = eval(display.innerHTML);
-  console.log(estado,"igual");
+  if(currentvalue == "On"){
+    display.innerHTML = eval(display.innerHTML);
+    console.log(estado,"igual");
+    bip.play();
+  }
 }
 
-//-- Poner a cero la expresion
+// Poner a cero la expresion
 clear.onclick = (ev) => {
-  display.innerHTML = "0";
-  estado = ESTADO.INIT;
-  console.log(estado,"poner a 0");
+  if(currentvalue == "On"){
+    display.innerHTML = "0";
+    estado = ESTADO.INIT;
+    console.log(estado,"poner a 0");
+    bip.play();
+  }
 }
