9bbb12e67ed865cbc3056319233daa38531656f2
saraft <s.fernandezt.2017@alumnos.urjc.es>
2021-04-20 14:37:00 +0200

    movimiento horizontal bola

--- P3/index.js
+++ P3/index.js
@@ -9,34 +9,54 @@ canvas.height = 600;
 //-- Obtener el contexto del canvas
 const ctx = canvas.getContext("2d");
 //-- Posición del elemento a animar
-let x = 260;
-let y = 550;
+let xtabla = 260;
+let ytabla = 550;
+let xbola = 300;
+let ybola = 300;
 
+//-- Velocidades del objeto(de la bola)
+let velx = 3;
+let vely = 1;
+
+function dibujartabla(){
+    ctx.beginPath();
+    ctx.rect(xtabla, ytabla, 80, 20); //dibujar
+    ctx.fillStyle = 'blanchedalmond'; //estilo
+    ctx.fill(); //relleno
+    ctx.stroke() //dibujar el trazo
+  ctx.closePath();
+}
+function dibujarbola(){
+    ctx.beginPath();
+    ctx.arc(xbola, ybola, 10, 0, 2 * Math.PI); //dibujar
+    ctx.fillStyle = 'rgb(251, 255, 0)'; //estilo
+    ctx.fill(); //relleno
+    ctx.stroke() //dibuar el trazo
+  ctx.closePath();
+}
 //-- Funcion principal de animacion
 function update()
 {
     console.log("test");
   //-- Algoritmo de animacion:
-  //-- 1) Actualizar posiciones de los elementos
-  //-- Por hacer
+  
+  //-- movimiento bola
+  if (xbola < 0 || xbola >= (canvas.width - 20) ) {
+    velx = -velx;
+  }if(ybola <= 0 || ybola > 80) {
+    vely = -vely;
+  }
+  //-- Actualizar la posición
+  xbola = xbola + velx;
+  ybola = ybola + vely;
 
+  
   //-- 2) Borrar el canvas
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  //-- 3) Dibujar los elementos visibles
-  ctx.beginPath();
-    ctx.rect(x, y, 80, 20);
-
-    //-- Dibujar
-    ctx.fillStyle = 'blanchedalmond';
-
-    //-- Rellenar
-    ctx.fill();
-
-    //-- Dibujar el trazo
-    ctx.stroke()
-  ctx.closePath();
-
+ 
+    dibujartabla()
+    dibujarbola()
   //-- 4) Volver a ejecutar update cuando toque
   requestAnimationFrame(update);
 }
