a44a97d2a6ab24611aa7acf4909316bd7d874c33
saraft <s.fernandezt.2017@alumnos.urjc.es>
2021-04-20 13:43:05 +0200

    icono y letra

--- P3/index.js
+++ P3/index.js
@@ -5,3 +5,41 @@ const canvas = document.getElementById("canvas");
 //-- Definir el tamaño del canvas
 canvas.width = 600;
 canvas.height = 600;
+
+//-- Obtener el contexto del canvas
+const ctx = canvas.getContext("2d");
+//-- Posición del elemento a animar
+let x = 260;
+let y = 550;
+
+//-- Funcion principal de animacion
+function update()
+{
+    console.log("test");
+  //-- Algoritmo de animacion:
+  //-- 1) Actualizar posiciones de los elementos
+  //-- Por hacer
+
+  //-- 2) Borrar el canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  //-- 3) Dibujar los elementos visibles
+  ctx.beginPath();
+    ctx.rect(x, y, 80, 20);
+
+    //-- Dibujar
+    ctx.fillStyle = 'blanchedalmond';
+
+    //-- Rellenar
+    ctx.fill();
+
+    //-- Dibujar el trazo
+    ctx.stroke()
+  ctx.closePath();
+
+  //-- 4) Volver a ejecutar update cuando toque
+  requestAnimationFrame(update);
+}
+
+//-- ¡Que empiece la función!
+update();
