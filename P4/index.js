9c5535c18801b8d6887afee9edac09923085a38b
saraft <s.fernandezt.2017@alumnos.urjc.es>
2021-05-07 00:35:45 +0200

    añado desliz. y funcionan pero solo para una img

--- P4/index.js
+++ P4/index.js
@@ -10,11 +10,15 @@ const img1 = document.getElementById('image1');
 const img2 = document.getElementById('image2')
 const ctx = canvas.getContext('2d');
 
-//-- Acceso al deslizador
-const deslizador = document.getElementById('deslizador');
+//-- Acceso a los deslizadores
+const desRojo = document.getElementById('rojo');
+const desVerde = document.getElementById('verde');
+const desAzul = document.getElementById('azul');
 
-//-- Valor del deslizador
-const range_value = document.getElementById('range_value');
+//-- Valor de los deslizadores
+const value_rojo = document.getElementById('valueR');
+const value_verde = document.getElementById('valueG');
+const value_azul = document.getElementById('valueB');
 
 
 //imagen de antes 
@@ -41,48 +45,77 @@ image2.onclick = () => {
 //Función de retrollamada de imagen cargada
 //una vez pinchado en la imagen la cargo en el canvas 
 img1.onload = function(){
-    canvas.width = img1.width;
-    canvas.height = img1.height;
+    canvas.width = 450;
+    canvas.height = 300;
     ctx.drawImage(img1, 0, 0);
     console.log("Imagen1 ready");
   };
 
 img2.onload = function(){
-    canvas.width = img2.width;
-    canvas.height = img2.height;
+    canvas.width = 450;
+    canvas.height = 300;
     ctx.drawImage(img2, 0, 0);
     console.log("Imagen2 ready");
 };
 
 
-//-- Funcion de retrollamada del deslizador
-deslizador.oninput = () => {
-    //-- Mostrar el nuevo valor del deslizador
-    range_value.innerHTML = deslizador.value;
-  
-    //-- Situar la imagen original en el canvas
-    //-- No se han hecho manipulaciones todavia
-    
-    //-- Obtener la imagen del canvas en pixeles
-    let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-  
-    //-- Obtener el array con todos los píxeles
-    let data = imgData.data
-  
-    //-- Obtener el umbral de rojo del desliador
-    umbral = deslizador.value
-  
-    //-- Filtrar la imagen según el nuevo umbral
-    for (let i = 0; i < data.length; i+=4) {
-      if (data[i] > umbral)
-        data[i] = umbral;
+function filtroColores(data){
+  var umbralR = desRojo.value;
+  var umbralG = desVerde.value;
+  var umbralB = desAzul.value;
+  //-- Filtrar la imagen según el nuevo umbral
+  for (var i = 0; i < data.length; i+=4) {
+      if (data[i] > umbralR){
+        data[i] = umbralR;
+      }
+      if (data[i+1] > umbralG){
+        data[i+1] = umbralG;
+      }
+      if (data[i+2] > umbralB){
+        data[i+2] = umbralB;
+      }
     }
+}
+
+function deslizadores(){
+  ctx.drawImage(img1, 0,0); //retrollamada de los deslizadores
   
-    //-- Poner la imagen modificada en el canvas
+  desRojo.oninput = () => {
+    
+    value_rojo.innerHTML = desRojo.value;//muestro el nuevo valor del deslizador
+    
+    ctx.drawImage(img1, 0,0); //Situo la imagen 1 en el canvas
+    
+    let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);//Obtener la imagen del canvas en pixeles
+    
+    let data = imgData.data; //Obtener el array con todos los píxeles
+    
+    filtroColores(data); // Obtener el umbral del rojo según del deslizador
+    //Insertar imagen modificada en el canvas
     ctx.putImageData(imgData, 0, 0);
   }
 
+  desVerde.oninput = () => {
+    value_verde.innerHTML = desVerde.value;
+    ctx.drawImage(img1, 0,0);
+    let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    let data = imgData.data
+    //-- Obtener el umbral del del deslizador
+    filtroColores(data);
+    ctx.putImageData(imgData, 0, 0);
+  }
 
+  desAzul.oninput = () => {
+    value_azul.innerHTML = desAzul.value;
+    ctx.drawImage(img1, 0,0);
+    let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    let data = imgData.data
+    //-- Obtener el umbral del azul del deslizador
+    filtroColores(data);
+    ctx.putImageData(imgData, 0, 0);
+  }
+}
+deslizadores();
 console.log("Fin...");
 
 
